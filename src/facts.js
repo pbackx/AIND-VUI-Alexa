a7d1b5c5dd4889e227e5f54c593e7df695cf5620
Peter Backx <peter.backx@gmail.com>
2017-07-01 15:07:06 +0200

    Finishing part 1 of the project

--- src/facts.js
+++ src/facts.js
@@ -2,11 +2,25 @@
 // Include at least 10 distinct facts with a 4-digit year included in the text for each
 module.exports = {
     "FACTS_EN": [
-        "The field of AI is considered to have its origin in 1950, with publication of British mathematician Alan Turing's paper, Computing Machinery and Intelligence.",
-        "The term, Artificial Intelligence, was coined in 1956 by mathematician and computer scientist John McCarthy, at Dartmouth College, in New Hampshire.",
-        "In 1997, a chess-playing program named Deep Blue, developed by IBM, beat the reigning world chess champion.",
-        "A so-called AI Winter occurred in 1974, when funding was cut after Speech Understanding research did not live up to its promise.",
-        "A driverless robotic car named Stanley, engineered by Sebastian Thrun's Stanford Racing team, sped through the Mojave desert at 22 miles per hour to win the 2005 Darpa Grand Challenge.",
-        "Replace me and add some more facts!"
+        "The British Academy Games Awards chose Golden Eye 007 as the game of the year 1997.",
+        "In 1998, the game press universally chose The Legend of Zelda: Ocarina of Time as the best game of the year.",
+        "Eurogamer's game of the year in 1999 was Unreal Tournament.",
+        "The year 2000 did not have a clear winner. Tony Hawk's Pro Skater 2 was chosen as game of the year by Game Informer and number of other publications.",
+        "Grand Theft Auto 3 was GameSpot's and many other's game of the year in 2001.",
+        "GameFAQs readers voted Metroid Prime to be their game of the year in 2002.",
+        "2003 was the year of The Legend of Zelda: The Wind Waker. The press unanimously agreed that this was the best game.",
+        "2004 was a good year for gaming. Half-Life 2 was one of the more popular picks for game of the year. Grand Theft Auto: San Andreas was the main contender.",
+        "Resident Evil 4 made 2005 the year of the horror game.",
+        "2006 is the second year that a Legend of Zelda game was picked as game of the year. This time, Twilight Princess got the accolades.",
+        "In 2007, PC Gaming was really popular. Most publications chose Bioshock or Portal as their game of the year.",
+        "The return of the Fallout franchise, Fallout 3, was game of the year in 2008.",
+        "2009 did not have a clear winner. Kotaku and a few others picked Uncharted 2: Among Thieves.",
+        "X-Play chose Mass Effect as its 2010 game of the year.",
+        "The Elder Scrolls 5: Skyrim was a popular choice for game of the year 2011.",
+        "In 2012, the gaming press did not agree on their favorite game of the year. GameSpy chose X-Com Enemy Unknown.",
+        "The Last of Us was widely acclaimed as game of the year in 2013.",
+        "In 2014, Dragon Age: Inquisition was the most popular choice for game of the year.",
+        "The third installment of The Witcher, named Wild Hunt was game of the year in 2015.",
+        "In 2016, Electronic Gaming Monthly picked Overwatch as their game of the year."
     ]
 }
